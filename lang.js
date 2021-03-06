771105136169915341acb69a6cc8a39a6d7640af
Uto <daffamadeo@gmail.com>
2021-03-06 12:40:53 +0700

    added languages and record page

---
+++ lang.js
@@ -0,0 +1,48 @@
+var currLang = document.getElementsByClassName("currLang");
+currLang[0].innerHTML = getCookie("lang"); 
+
+var circleBorder;
+
+setBorder();
+
+function setBorder(){
+  var langName = currLang[0].innerHTML.substring(0, 3);
+  langName = langName.toLowerCase();
+  
+  circleBorder = document.getElementsByClassName(langName);
+  
+  circleBorder[0].classList.add("selected");
+}
+
+function setLang(langChoice){
+
+    // ADD BORDER WHEN SELECTED //
+    circleBorder[0].classList.remove("selected");
+    setCookie('lang',langChoice,90);
+    currLang[0].innerHTML = getCookie("lang"); 
+    setBorder();
+}
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+    var expires = 'expires=' + d.toUTCString();
+    document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+  }
+
+function getCookie(cname) {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+  
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  }
---
+++ records.js
@@ -0,0 +1,34 @@
+var types = ["15","30", "50", "100", "200"];
+types.forEach( e=>{
+    var wpmRecord = document.getElementById(`${e}-wpm`);
+    var accRecord = document.getElementById(`${e}-acc`);
+    var dateRecord = document.getElementById(`${e}-date`);
+
+    const wpmCurr = getCookie(`${e}-wpm`);
+    const accCurr = getCookie(`${e}-acc`);
+    const dateCurr = getCookie(`${e}-date`);
+    
+    if(wpmCurr !== ''){
+        wpmRecord.innerHTML = wpmCurr;
+        accRecord.innerHTML = accCurr+"%";
+        dateRecord.innerHTML = dateCurr;
+    }
+})
+
+function getCookie(cname) {
+  var name = cname + '=';
+  var decodedCookie = decodeURIComponent(document.cookie);
+  var ca = decodedCookie.split(';');
+
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      console.log( c.substring(name.length, c.length))
+      return c.substring(name.length, c.length);
+    }
+  }
+  return '';
+}
