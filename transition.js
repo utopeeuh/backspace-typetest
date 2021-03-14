912ff3c9b844509fcfa10970b9e3d37310e3db30
Uto <daffamadeo@gmail.com>
2021-03-14 16:42:42 +0700

    Add files via upload

---
+++ transition.js
@@ -0,0 +1,57 @@
+let theme;
+getCookie('theme') === '' ? theme = 'dark' : theme = (getCookie('theme'));
+setThemeTrans(theme);
+
+function setThemeTrans(_theme) {
+  console.log(theme);
+  fetch(`./themes/${_theme}.css`)
+    .then(response => {
+      if (response.status === 200) {
+        response
+          .text()
+          .then(css => {
+            setCookie('theme', theme, 90);
+            document.querySelector('#theme').setAttribute('href', `themes/${theme}.css`);
+          })
+          .catch(err => console.error(err));
+      } else {
+        console.log(`theme ${theme} is undefine`);
+      }
+    })
+.catch(err => console.error(err));
+}
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
+
+function setCookie(cname, cvalue, exdays) {
+  var d = new Date();
+  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
+  var expires = 'expires=' + d.toUTCString();
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/';
+}
+
+function getCookie(cname) {
+    var name = cname + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+  
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') {
+        c = c.substring(1);
+      }
+      if (c.indexOf(name) == 0) {
+        return c.substring(name.length, c.length);
+      }
+    }
+    return '';
+  }
